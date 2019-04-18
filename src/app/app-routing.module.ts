df35cea7046dadf04071f910b24a144f529b8bc0
shubham <shubhammane56@gmail.com>
2019-04-18 15:20:31 +0530

    changes from derrick

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetailedProductsComponent } from './detailed-products/detailed-products.component';
 import { HomeComponent } from './home/home.component';
+import { FeaturedProductsComponent } from './featured-products/featured-products.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path: 'allproducts',component:DetailedProductsComponent}
+  {path:'app-home',component:HomeComponent},
+  {path: 'allproducts',component:DetailedProductsComponent},
+  {path: 'products',component:FeaturedProductsComponent}
 ];
 
 @NgModule({
--- src/app/featured-products/featured-products.component.ts
+++ src/app/featured-products/featured-products.component.ts
@@ -39,6 +39,8 @@ export class FeaturedProductsComponent implements OnInit {
     });
   }
 
+  
+
   ngOnInit() {
   
   }
